407b381bb443efdf4092c3c08eacd0588ec72e5b
Aymeric Vaneme <mric.vaneme@hotmail.fr>
2016-05-04 17:19:30 +0200

    Week-end

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading"><h4>Dashboard</h4></div>
+                <div class="panel-heading"><h4>Bienvenue " {{Auth::user()->name}} "</h4></div>
 
                 <div class="panel-body">
 
@@ -21,12 +21,14 @@
                                             {{round($percentTotalGazette,2)}}%
                                         </div>
                                     </div>
+                                    <hr>
                                     <label>Verticaux</label>
                                     <div class="progress">
                                         <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="{{round($percentTotalVerticaux,2)}}" aria-valuemin="0" aria-valuemax="100" style="min-width: 2em;width: {{$percentTotalVerticaux}}%">
                                             {{round($percentTotalVerticaux,2)}}%
                                         </div>
                                     </div>
+                                    <hr>
                                     <label>Evenement</label>
                                     <div class="progress">
                                         <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="min-width: 2em;width: {{$percentTotalEvenement}}%">
@@ -47,6 +49,8 @@
                                             {{round(($avenants->sum('realise'))/$avenants->sum('objectif')*100, 2)}}%
                                         </div>
                                     </div>
+                                    <hr>
+                                    <br>
                                     <table class="table">
                                         <tr>
                                             <td>Nombres de ventes :</td>
