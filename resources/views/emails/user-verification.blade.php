8d8215b086c407c757098d91c338b1c863a7f065
devnem <nempointcom@hotmail.com>
2016-05-14 10:55:18 +0200

    register mail UI

--- resources/views/emails/user-verification.blade.php
+++ resources/views/emails/user-verification.blade.php
@@ -1,2 +1,22 @@
-Click here to verify your account: 
-<a href="{{ $link = url('verification', $user->verification_token) . '?email=' . urlencode($user->email) }}"> {{ $link }}</a>
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Register</title>
+</head>
+<body>
+<div>
+    <h3>Bonjour, et merci de vous être inscrit</h3>
+    <hr>
+    <label>Validation de compte</label>
+    <hr>
+    <p>Cliquez sur le lien suivant pour valider votre compte :
+        <a href="{{ $link = url('verification', $user->verification_token) . '?email=' . urlencode($user->email) }}"> {{ $link }}</a>
+    </p>
+    <p>
+        Signé : l'administrateur
+    </p>
+
+</div>
+</body>
+</html>
