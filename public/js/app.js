9270b874767765eb6cfb8fbada56f321ff206a78
Aymeric Vaneme <mric.vaneme@hotmail.fr>
2016-05-24 16:06:04 +0200

    chartjs bonus bar stacked

--- public/js/app.js
+++ public/js/app.js
@@ -2,6 +2,27 @@ $(document).ready(function() {
 
     // $('#myTable').DataTable();
 
+    console.log(trim1);
+    console.log(trim2);
+    var bonus1 = 0
+    var bonus2 = 0
+    var bonus3 = 0
+
+    if(trim1 > 100){
+        bonus1 =trim1-100;
+        trim1= 100;         
+    }
+    if(trim2+bonus1 >100){
+        bonus2= trim2+bonus1-100;
+        bonus1=0;
+        trim2 = 100;
+    }
+    if(trim3+bonus2 >100){
+        bonus3= trim3+bonus2-100;
+        bonus2=0;
+        trim3 = 100;
+    }
+
     var ctx1 = document.getElementById("trimestre");
     var myChart1 = new Chart(ctx1, {
         type: 'bar',
@@ -10,12 +31,21 @@ $(document).ready(function() {
             datasets: [{
                 label: '% Atteints',
                 backgroundColor: "#5CB85C",
-                data: [trim1, trim2, trim3, trim4]
+                data: [trim1, trim2, trim3, trim4],
+
+            },{
+                label:'% bonus',
+                backgroundColor: '#F2FF29',
+                data:[0,bonus1,bonus2,bonus3]
             }]
         },
         options: {
             scales: {
+                xAxes: [{
+                    stacked:true,
+                }],
                 yAxes: [{
+                    stacked:true,
                     ticks: {
                         beginAtZero: true,
                         max: 100
